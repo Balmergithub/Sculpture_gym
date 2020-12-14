bea4c89b9cb4e12af9567e3d1b83a8705cea8b56
balmergithub <mwongeraligo@gmail.com>
2020-12-14 17:58:38 +0300

    few attritubes changed

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name', 'indianabeautyandcosmetics')}}</title>
+
+        
+    </head>
+    <body>
+         <h1>Welcome to Beauty and Cosmetics</h1>
+         <p>this my beauty and products website.</p>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {  //returns the string "welcome" to the web page
+//     return view('welcome');
+// });
+
+// });
+// Route::get('/users/{id}/{name}', function ($id, $name) {  //**** returns the inputed VALUES on the url box to the web page
+//     return 'This is user '.$name.' with the id of '.$id;
+// });
+
+Route::get('/','App\Http\Controllers\PagesController@index');
+
+Route::get('/about', function () {     //**** returns the file in the view/pages/about file
+    return view('pages.about');
 });
+     
